a3bc0d5d1f4a7fc3a58b95f2962d10701d0f537c
unknown <Philippe Chretien@.(none)>
2010-04-16 22:53:32 -0400

    Added the CryptoNotes plugin with no item editor dialog.

---
+++ CryptoNotes/CryptoEditorNotes.cs
@@ -0,0 +1,35 @@
+﻿using CryptoEditor.Common;
+using CryptoEditor.FormFramework;
+
+namespace CryptoNotes
+{
+    [CryptoEditorPlugin("Notes")]
+    public class CryptoEditorNotes : CryptoEditorPlugin<CryptoEditorNotesItem>
+    {
+        public CryptoEditorNotes()
+        {
+            this.Detail = new CryptoEditorPluginDetailList<CryptoEditorNotesItem>(this);
+        }
+
+        public override object CreateItem()
+        {
+            //
+            // A new form must be implemented by the user to get item details ...
+            //
+            CryptoEditorNotesItem item = new CryptoEditorNotesItem("name - " + System.Guid.NewGuid(),
+                "value - " + System.Guid.NewGuid());
+
+            base.CreateItem();
+            return item;
+        }
+
+        public override object UpdateItem(object itemIn)
+        {
+            CryptoEditorNotesItem item = (CryptoEditorNotesItem)itemIn;
+            item.Note += "<*>";
+
+            base.UpdateItem(item);
+            return item;
+        }
+    }
+}
---
+++ CryptoNotes/CryptoEditorNotesItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CryptoEditor.Common;
+
+namespace CryptoNotes
+{
+    [Serializable]
+    public class CryptoEditorNotesItem : CryptoEditorPluginItem
+    {
+        public CryptoEditorNotesItem()
+        {
+        }
+
+        public CryptoEditorNotesItem(string title, string note)
+        {
+            Title = title;
+            Note = note;
+        }
+
+        private string title = "";
+        [CryptoEditorPluginItem(0, Header = "Title", Width = 100)]
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string note = "";
+        [CryptoEditorPluginItem(1, Header="Note", Width=400)]
+        public string Note
+        {
+            get { return note; }
+            set { note = value; }
+        }
+    }
+}
