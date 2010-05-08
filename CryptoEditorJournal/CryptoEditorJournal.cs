17d2b681766d485b03dd2bea60e309317e258367
Philippe Chretien <philippe.chretien@gmail.com>
2010-05-07 23:34:27 -0400

    Added the bases for the Journal plugin

---
+++ CryptoEditorJournal/CryptoEditorJournal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CryptoEditor.Common;
+using CryptoEditor.FormFramework;
+
+namespace CryptoEditor.Journal
+{
+    [CryptoEditorPlugin("Journal")]
+    class CryptoEditorJournal : CryptoEditorPlugin<CryptoEditorJournalItem>
+    {
+        public CryptoEditorJournal()
+        {
+            this.Detail = new CryptoEditorPluginDetailList<CryptoEditorJournalItem>(this);
+        }
+
+        public override object CreateItem()
+        {
+            //
+            // A new form must be implemented by the user to get item details ...
+            //
+            CryptoEditorJournalItem item = new CryptoEditorJournalItem(DateTime.Now, "Title " + System.Guid.NewGuid(),
+                "Text " + System.Guid.NewGuid());
+
+            base.CreateItem();
+            return item;
+        }
+
+        public override object UpdateItem(object itemIn)
+        {
+            CryptoEditorJournalItem item = (CryptoEditorJournalItem)itemIn;
+            item.Title += "<*>";
+
+            base.UpdateItem(item);
+            return item;
+        }
+
+        public override bool IsSearchable()
+        {
+            return true;
+        }
+    }
+}
---
+++ CryptoEditorJournal/CryptoEditorJournalItem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CryptoEditor.Common;
+
+namespace CryptoEditor.Journal
+{
+    [Serializable]
+    public class CryptoEditorJournalItem : CryptoEditorPluginItem
+    {
+        public CryptoEditorJournalItem()
+        {
+        }
+
+        public CryptoEditorJournalItem(DateTime date, string title, string text)
+        {
+            Date = date;
+            Title = title;
+            Text = text;
+        }
+
+        private DateTime date;
+        [CryptoEditorPluginItem(0, Header = "Date", Width = 100)]
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        private string title = "";
+        [CryptoEditorPluginItem(1, Header = "Title", Width = 250, Searchable = true)]
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string notes = "";
+        [CryptoEditorPluginItem(Header = "Text", Searchable = true)]
+        public string Text
+        {
+            get
+            {
+                if (!Serializing)
+                    return notes;
+
+                // Note: This could be a function in the framework
+                string ret = notes.Replace("\n", "{{N}}");
+                ret = ret.Replace("\r", "{{R}}");
+
+                return ret;
+            }
+            set
+            {
+                if (!Serializing)
+                {
+                    notes = value;
+                    return;
+                }
+
+                // Note: This could be a function in the framework
+                string input = value.Replace("{{N}}", "\n");
+                input = input.Replace("{{R}}", "\r");
+
+                notes = input;
+            }
+        }
+    }
+}
